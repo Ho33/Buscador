e9e7c88f14398982a75a823cba5840425f42e898
Mikel Lopez <mikel_lopez_salazar@carrefour.com>
2020-10-20 13:16:16 +0200

    Initial Commit

---
+++ Buscador_Tiendas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buscador_Tiendas
+//
+//  Created by usuario on 20/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
